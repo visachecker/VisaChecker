b5607446ac5390646d11426fe3bfcc83caad3047
YevgeniyFomin <yevgeniy.fomin@buhlergroup.com>
2022-11-15 13:59:08 +0100

    Correct fields created, UI changed

--- src/components/registrationForm.js
+++ src/components/registrationForm.js
@@ -1,80 +1,115 @@
-import React, {useState,setState} from 'react';
-import './style.css'
-import {database} from '../firebase'
-import {ref,push,child,update} from "firebase/database";
+import React, { useState } from 'react';
+import './style.css';
+import { database } from '../firebase';
+import { ref, push, child, update } from 'firebase/database';
+
 function RegistrationForm() {
-    
-    const [firstName, setFirstName] = useState(null);
-    const [lastName, setLastName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [password,setPassword] = useState(null);
-    const [confirmPassword,setConfirmPassword] = useState(null);
+  const [appNum, setAppNum] = useState(null);
+  const [appNumFake, setAppNumFake] = useState(null);
+  const [appCode, setAppCode] = useState(null);
+  const [appYear, setAppYear] = useState(null);
+  const [email, setEmail] = useState(null);
 
-    const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id === "firstName"){
-            setFirstName(value);
-        }
-        if(id === "lastName"){
-            setLastName(value);
-        }
-        if(id === "email"){
-            setEmail(value);
-        }
-        if(id === "password"){
-            setPassword(value);
-        }
-        if(id === "confirmPassword"){
-            setConfirmPassword(value);
-        }
-
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === 'appNum') {
+      setAppNum(value);
     }
-
-    const handleSubmit  = () => {
-        let obj = {
-            firstName : firstName,
-            lastName:lastName,
-            email:email,
-            password:password,
-            confirmPassword:confirmPassword,
-        }       
-        const newPostKey = push(child(ref(database), 'posts')).key;
-        const updates = {};
-        updates['/' + newPostKey] = obj
-
-        return update(ref(database), updates);
+    if (id === 'apppNumFake') {
+      setAppNumFake(value);
     }
+    if (id === 'appCode') {
+      setAppCode(value);
+    }
+    if (id === 'appYear') {
+      setAppYear(value);
+    }
+    if (id === 'email') {
+      setEmail(value);
+    }
+  };
 
-    return(
-        <div className="form">
-            <div className="form-body">
-                <div className="username">
-                    <label className="form__label" for="firstName">First Name </label>
-                    <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" placeholder="First Name"/>
-                </div>
-                <div className="lastname">
-                    <label className="form__label" for="lastName">Last Name </label>
-                    <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="LastName"/>
-                </div>
-                <div className="email">
-                    <label className="form__label" for="email">Email </label>
-                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
-                </div>
-                <div className="password">
-                    <label className="form__label" for="password">Password </label>
-                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
-                </div>
-                <div className="confirm-password">
-                    <label className="form__label" for="confirmPassword">Confirm Password </label>
-                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
-                </div>
-            </div>
-            <div class="footer">
-                <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
-            </div>
+  const handleSubmit = () => {
+    let obj = {
+      appNum: appNum,
+      appNumFake: appNumFake,
+      appCode: appCode,
+      appYear: appYear,
+      email: email,
+    };
+    const newPostKey = push(child(ref(database), 'posts')).key;
+    const updates = {};
+    updates['/' + newPostKey] = obj;
+
+    return update(ref(database), updates);
+  };
+
+  return (
+    <div className='form'>
+      <div className='form-body'>
+        <div className='form-row'>
+          <label className='form__label'>Application number </label>
+          <input
+            type='text'
+            id='appNum'
+            className='form__input'
+            value={appNum}
+            onChange={(e) => handleInputChange(e)}
+            placeholder='12345*'
+          />
         </div>
-       
-    )       
+        <div className='form-row'>
+          <label className='form__label'>-XX </label>
+          <input
+            type='text'
+            id='appNumFake'
+            className='form__input'
+            value={appNumFake}
+            onChange={(e) => handleInputChange(e)}
+            placeholder='-XX'
+          />
+        </div>
+        <div className='form-row'>
+          <label className='form__label'>CC* </label>
+          <input
+            type='text'
+            id='appCode'
+            className='form__input'
+            value={appCode}
+            onChange={(e) => handleInputChange(e)}
+            placeholder='Typ řízení (СС)*'
+          />
+        </div>
+        <div className='form-row'>
+          <label className='form__label'>YYYY* </label>
+          <input
+            type='text'
+            id='appYear'
+            className='form__input'
+            value={appYear}
+            onChange={(e) => handleInputChange(e)}
+            placeholder='Rok'
+          />
+        </div>
+        <div className='form-row'>
+          <label className='form__label'>Email </label>
+          <input
+            type='email'
+            id='email'
+            className='form__input'
+            value={email}
+            onChange={(e) => handleInputChange(e)}
+            placeholder='example@email.com'
+          />
+        </div>
+      </div>
+      <div class='footer'>
+        <button type='submit' className='btn' onClick={() => handleSubmit()}>
+          Register
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default RegistrationForm
+export default RegistrationForm;
