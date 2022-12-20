99a3c26613b9ab27b810fe63976a5c087b1fe545
Ivan Manoilov <i.o.manoilov@gmail.com>
2022-12-20 22:47:32 +0100

    Refactor status parsing

--- VisaChecker.Server/src/main/java/com/visachecker/server/status/infrastructure/http/StatusResponseParser.java
+++ VisaChecker.Server/src/main/java/com/visachecker/server/status/infrastructure/http/StatusResponseParser.java
@@ -5,9 +5,19 @@ import com.visachecker.server.status.domain.Status;
 import com.visachecker.server.status.domain.exceptions.ApplicationNotFoundException;
 import org.jsoup.nodes.Document;
 
+import java.util.Map;
+
 
 public class StatusResponseParser {
     private final Document document;
+    private final String containingElementTag = "strong";
+    private final String containingElementValueNotFoundText = "Nenalezeno";
+
+    private final Map<Decision, String> decisionToElementTextMap = Map.of(
+            Decision.APPROVED, "Vyřízeno – POVOLENO",
+            Decision.DECLINED, "Vyřízeno – NEPOVOLENO",
+            Decision.IN_PROGRESS, "Zpracovává se"
+    );
 
     public StatusResponseParser(Document document) {
         this.document = document;
@@ -22,17 +32,18 @@ public class StatusResponseParser {
     }
 
     public Status getStatus() throws ApplicationNotFoundException {
-        var element = document.selectXpath("//section[@class='col-sm-12'][1]/div[1]/ul/li[1]/p/span/strong").first();
-        if (element != null) {
-            var state = element.childNode(0).attributes().get("#text");
-            return new Status(switch (state) {
-                case "Vyřízeno – POVOLENO" -> Decision.APPROVED;
-                case "Vyřízeno – NEPOVOLENO" -> Decision.DECLINED;
-                case "Zpracovává se" -> Decision.IN_PROGRESS;
-                case "Nenalezeno" -> throw new ApplicationNotFoundException();
-                default -> throw new IllegalArgumentException();
-            });
+        for (var decision : decisionToElementTextMap.keySet()) {
+            if (statusElementContainsText(decisionToElementTextMap.get(decision))) {
+                return new Status(decision);
+            }
         }
+
+        if (statusElementContainsText(containingElementValueNotFoundText)) throw new ApplicationNotFoundException();
+
         throw new IllegalStateException("No status element in status response");
+    }
+
+    private boolean statusElementContainsText(String text) {
+        return document.select("%s:contains(%s)".formatted(containingElementTag, text)).size() == 1;
     }
 }
